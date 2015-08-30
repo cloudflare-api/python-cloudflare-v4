efefa62c5c94f7e0aa654f974f9d760190f1868b
Felix Wong <felix@fawong.com>
2015-08-30 01:57:13 -0700

    fix post and delete + add purge_cache

--- cloudflare_v4/__init__.py
+++ cloudflare_v4/__init__.py
@@ -15,7 +15,7 @@ class CloudFlare(object):
             self.TOKEN = token
             self.logger = logger.Logger(debug).getLogger()
 
-        def call(self, method, main_endpoint, endpoint=None, params=None):
+        def call(self, method, main_endpoint, endpoint=None, params=None, data=None):
             headers = { "X-Auth-Email": self.EMAIL, "X-Auth-Key": self.TOKEN }
             if endpoint is not None:
                 url = BASE_URL + '/' +  main_endpoint + '/' + params + '/' + endpoint
@@ -27,6 +27,7 @@ class CloudFlare(object):
             self.logger.debug("method type is: " + method)
             self.logger.debug("url endpoint is: " + url)
             self.logger.debug("optional params is: " + str(params))
+            self.logger.debug("optional data is: " + str(data))
             if (method is None) or (main_endpoint is None):
                 raise CloudFlareError('You must specify a method and endpoint') # should never happen
             else:
@@ -35,9 +36,9 @@ class CloudFlare(object):
                     response = requests.get(url, headers=headers, params=params)
                 elif method == 'POST':
                     headers['Content-Type'] = 'application/json'
-                    response = requests.post(url, headers=headers, json=params)
+                    response = requests.post(url, headers=headers, json=data)
                 elif method == 'DELETE':
-                    response = requests.delete(url, headers=headers, json=params)
+                    response = requests.delete(url, headers=headers, json=data)
                 data = response.text
                 self.logger.debug("data received: " + data)
                 try:
@@ -51,6 +52,9 @@ class CloudFlare(object):
 
     class DynamicClient:
         def __init__(self, base_client, main_endpoint, endpoint=None):
+            base_client.logger.debug("base client is: " + str(base_client))
+            base_client.logger.debug("main endpoint is: " + str(main_endpoint))
+            base_client.logger.debug("endpoint is: " + str(endpoint))
             self.base_client = base_client
             self.main_endpoint = main_endpoint
             self.endpoint = endpoint
@@ -59,17 +63,20 @@ class CloudFlare(object):
             return self.base_client.call('GET', self.main_endpoint,
                 self.endpoint, params)
 
-        def post(self, params=None):
+        def post(self, params=None, data=None):
             return self.base_client.call('POST', self.main_endpoint,
-                self.endpoint, params)
+                self.endpoint, params, data)
 
-        def delete(self, params=None):
+        def delete(self, params=None, data=None):
             return self.base_client.call('DELETE', self.main_endpoint,
-                self.endpoint, params)
+                self.endpoint, params, data)
 
     def __init__(self, email, token, debug):
         self.base_client = self.BaseClient(email, token, debug)
         setattr(self, "zones", self.DynamicClient(self.base_client, "zones"))
         setattr(self, "user", self.DynamicClient(self.base_client, "user"))
         zones = getattr(self, "zones")
-        setattr(zones, "dns_records", self.DynamicClient(self.base_client, "zones", "dns_records"))
+        setattr(zones, "dns_records", self.DynamicClient(self.base_client,
+            "zones", "dns_records"))
+        setattr(zones, "purge_cache", self.DynamicClient(self.base_client,
+            "zones", "purge_cache"))
