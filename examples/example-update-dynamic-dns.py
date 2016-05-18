3225f64e94e188362c5ed5c71c09d1a9207871e3
mahtin <mahtin@mahtin.com>
2016-05-18 05:46:39 -0700

    Cleanup of code and confirmed functionality with A/AAAA records present

--- examples/example-update-dynamic-dns.py
+++ examples/example-update-dynamic-dns.py
@@ -29,6 +29,65 @@ def my_ip_address():
 
 	return ip_address, ip_address_type
 
+def do_dns_update(cf, zone_name, zone_id, dns_name, ip_address, ip_address_type):
+	try:
+		params = {'name':dns_name,'match':'all','type':ip_address_type}
+		dns_records = cf.zones.dns_records.get(zone_id, params=params)
+	except CloudFlare.CloudFlareAPIError as e:
+		exit('/zones/dns_records %s - %d %s - api call failed' % (dns_name, e, e))
+
+	updated = False
+
+	# update the record - unless it's already correct
+	for dns_record in dns_records:
+		old_ip_address = dns_record['content']
+		old_ip_address_type = dns_record['type']
+
+		if ip_address_type not in ['A', 'AAAA']:
+			# we only deal with A / AAAA records
+			continue
+
+		if ip_address_type != old_ip_address_type:
+			# only update the correct address type (A or AAAA)
+			# we don't see this becuase of the search params above
+			print 'IGNORED: %s %s ; wrong address family' % (dns_name, old_ip_address)
+			continue
+
+		if ip_address == old_ip_address:
+			print 'UNCHANGED: %s %s' % (dns_name, ip_address)
+			updated = True
+			continue
+
+		# Yes, we need to update this record - we know it's the same address type
+
+		dns_record_id = dns_record['id']
+		dns_record = {
+			'name':dns_name,
+			'type':ip_address_type,
+			'content':ip_address
+		}
+		try:
+			dns_record = cf.zones.dns_records.put(zone_id, dns_record_id, data=dns_record)
+		except CloudFlare.CloudFlareAPIError as e:
+			exit('/zones.dns_records.put %s - %d %s - api call failed' % (dns_name, e, e))
+		print 'UPDATED: %s %s -> %s' % (dns_name, old_ip_address, ip_address)
+		updated = True
+
+	if updated:
+		return
+
+	# no exsiting dns record to update - so create dns record
+	dns_record = {
+		'name':dns_name,
+		'type':ip_address_type,
+		'content':ip_address
+	}
+	try:
+		dns_record = cf.zones.dns_records.post(zone_id, data=dns_record)
+	except CloudFlare.CloudFlareAPIError as e:
+		exit('/zones.dns_records.post %s - %d %s - api call failed' % (dns_name, e, e))
+	print 'CREATED: %s %s' % (dns_name, ip_address)
+
 def main():
 	try:
 		dns_name = sys.argv[1]
@@ -52,6 +111,9 @@ def main():
 	except Exception as e:
 		exit('/zones.get - %s - api call failed' % (e))
 
+	if len(zones) == 0:
+		exit('/zones.get - %s - zone not found' % (zone_name))
+
 	if len(zones) != 1:
 		exit('/zones.get - %s - api call returned %d items' % (zone_name, len(zones)))
 
@@ -60,60 +122,8 @@ def main():
 	zone_name = zone['name']
 	zone_id = zone['id']
 
-	try:
-		params = {'name':dns_name,'match':'all','type':ip_address_type}
-		dns_records = cf.zones.dns_records.get(zone_id, params=params)
-	except CloudFlare.CloudFlareAPIError as e:
-		exit('/zones/dns_records %s - %d %s - api call failed' % (dns_name, e, e))
-
-	did_update = False
-
-	# update the record - unless it's already correct
-	for dns_record in dns_records:
-		old_ip_address = dns_record['content']
-		old_ip_address_type = dns_record['type']
-
-		if ip_address_type not in ['A', 'AAAA']:
-			# we only deal with A / AAAA records
-			continue
-
-		if ip_address_type != old_ip_address_type:
-			# only update the correct address type (A or AAAA)
-			print 'IGNORED: %s %s ; wrong address family' % (dns_name, old_ip_address)
-			continue
-
-		if ip_address == old_ip_address:
-			print 'UNCHANGED: %s %s' % (dns_name, ip_address)
-			did_update = True
-			continue
-
-		dns_record_id = dns_record['id']
-		# update this record - we know it's the same address type
-		dns_record = {
-			'name':dns_name,
-			'type':ip_address_type,
-			'content':ip_address
-		}
-		try:
-			dns_record = cf.zones.dns_records.put(zone_id, dns_record_id, data=dns_record)
-		except CloudFlare.CloudFlareAPIError as e:
-			exit('/zones.dns_records.put %s - %d %s - api call failed' % (dns_name, e, e))
-		print 'UPDATED: %s %s -> %s' % (dns_name, old_ip_address, ip_address)
-		did_update = True
-
-	if did_update == False:
-		# nothing found - so create record
-		dns_record = {
-			'name':host_name,
-			'type':ip_address_type,
-			'content':ip_address
-		}
-		try:
-			dns_record = cf.zones.dns_records.post(zone_id, data=dns_record)
-		except CloudFlare.CloudFlareAPIError as e:
-			exit('/zones.dns_records.post %s - %d %s - api call failed' % (dns_name, e, e))
-		print 'CREATED: %s %s' % (dns_name, ip_address)
-		exit(0)
+	do_dns_update(cf, zone_name, zone_id, dns_name, ip_address, ip_address_type)
+	exit(0)
 
 if __name__ == '__main__':
 	main()
